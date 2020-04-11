4af43485c8a11b53bd83f84ee15add345f8b5542
kageroZa <furkangumus@windowslive.com>
2020-04-11 19:53:19 +0300

    Studying with Syntetic Data

---
+++ First Week/lr_on_syntetic_data/.ipynb_checkpoints/lr_on_syntetic_data-checkpoint.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 11:08:41 2020
+
+@author: kage
+"""
+
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set default plot theme to seaborn.
+sns.set()
+
+
+def build_model(learning_rate):
+    
+    # most simple keras models are Sequential
+    # a sequential model contains one or more layers
+    model = tf.keras.models.Sequential()
+    
+    # describe the topography of the model.
+    # The topography of a simple linear regression model
+    # is a single node in a single layer.
+    model.add(tf.keras.layers.Dense(units=1, input_shape=(1,)))
+    
+    # Compile the model topography into code that 
+    # TensorFlow can efficiently execute. Configure 
+    # training to minimize the model's mean squared error. 
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=learning_rate),
+                loss="mean_squared_error",
+                metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+    return model
+
+
+def train_model(model, feature, label, epochs, batch_size):
+    """
+    Train the model by feeding it data.
+    """
+
+    # Feed the feature values and the label values to the 
+    # model. The model will train for the specified number 
+    # of epochs, gradually learning how the feature values
+    # relate to the label values. 
+    history = model.fit(x=feature,
+                      y=label,
+                      batch_size=batch_size,
+                      epochs=epochs)
+
+    # Gather the trained model's weight and bias.
+    trained_weight = model.get_weights()[0]
+    trained_bias = model.get_weights()[1]
+
+    # The list of epochs is stored separately from the 
+    # rest of history.
+    epochs = history.epoch
+  
+    # Gather the history (a snapshot) of each epoch.
+    hist = pd.DataFrame(history.history)
+
+    # Specifically gather the model's root mean 
+    #squared error at each epoch. 
+    rmse = hist["root_mean_squared_error"]
+
+    return trained_weight, trained_bias, epochs, rmse
+
+def plot_the_model(trained_weight, trained_bias, feature, label):
+    """
+    Plot the trained model against the training feature and label.
+    """
+
+    # Label the axes.
+    plt.xlabel("feature")
+    plt.ylabel("label")
+
+    # Plot the feature values vs. label values.
+    plt.scatter(feature, label)
+
+    # Create a red line representing the model. The red line starts
+    # at coordinates (x0, y0) and ends at coordinates (x1, y1).
+    x0 = 0
+    y0 = trained_bias
+    x1 = features[-1]
+    y1 = trained_bias + (trained_weight * x1)
+    plt.plot([x0, x1], [y0, y1], c='r')
+
+    # Render the scatter plot and the red line.
+    plt.show()
+
+def plot_the_loss_curve(epochs, rmse):
+    """
+    Plot the loss curve, which shows loss vs. epoch.
+    """
+
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Root Mean Squared Error")
+
+    plt.plot(epochs, rmse, label="Loss")
+    plt.legend()
+    plt.ylim([rmse.min()*0.97, rmse.max()])
+    plt.show()
+
+
+features = ([1.0, 2.0,  3.0,  4.0,  5.0,  6.0,  
+               7.0,  8.0,  9.0, 10.0, 11.0, 12.0])
+label   = ([5.0, 8.8,  9.6, 14.2, 18.8, 19.5, 
+               21.4, 26.8, 28.9, 32.0, 33.8, 38.2])
+
+learning_rate=0.025
+epoch_count=250 # how many epochs we want.
+batch_size=1
+
+model = build_model(learning_rate)
+trained_weight, trained_bias, epochs, rmse = train_model(model, features, 
+                                                         label, epoch_count,
+                                                         batch_size)
+plot_the_model(trained_weight, trained_bias, features, label)
+plot_the_loss_curve(epochs, rmse)
